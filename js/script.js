eb2b1127adaf8679454486edd62bd320c74b813c
Павел <pavlo.bogachenko@ukr.net>
2021-03-03 13:36:19 +0200

    last version

--- js/script.js
+++ js/script.js
@@ -10,7 +10,26 @@ let personalMovieDB = {
     privat: false
 };
 
+for (let i = 0; i < 1; i++) {
 let a = prompt('Один из последних просмотренных фильмов?')
 let b = prompt('На сколько оцените его?')
 
-personalMovieDB.movies[a] = b;
+    if(a != null && b !=null && a.length < 50 && a != '' && b != '') {
+        console.log('done')
+    } else {
+        console.log('error')
+        i--;
+    }
+
+        if (personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов!')
+    } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+        console.log('Вы классический зритель!') 
+    } else if (personalMovieDB.count > 30) {
+        console.log('Вы киноман!')
+    } else {
+        console.log('Error 404 BOY')
+    }
+    
+    personalMovieDB.movies[a] = b;
+};
